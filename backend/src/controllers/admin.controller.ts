def2027d2fa0a7f00c35869e6a35a0be3dc8018a
ThembaMtshelwane <thembamm3@gmail.com>
2025-05-09 11:34:16 +0200

    registration service added

--- backend/src/controllers/admin.controller.ts
+++ backend/src/controllers/admin.controller.ts
@@ -1,34 +1,16 @@
 import expressAsyncHandler from "express-async-handler";
 import { Request, Response } from "express";
 import Admin from "../model/users/admin.model";
-import { INTERNAL_SERVER_ERROR } from "../consts/http.codes";
-import HTTP_Error from "../utils/httpError";
+import { registerService } from "../services/regsiterService";
 
 export const registerAdmin = expressAsyncHandler(
   async (req: Request, res: Response) => {
-    const { firstName, lastName, email } = req.body;
-    const existingUser = await Admin.findOne({ email });
-    if (existingUser) {
-      throw new HTTP_Error("User already exists", INTERNAL_SERVER_ERROR);
-    }
-    const generatedPassword = "123";
-
-    const newUser = await Admin.create({
-      firstName,
-      lastName,
-      email,
-      password: generatedPassword,
-    });
-
-    if (!newUser) {
-      throw new HTTP_Error("Failed to create user", INTERNAL_SERVER_ERROR);
-    }
-
+    const user = await registerService(Admin, req.body);
     res.status(201).json({
       message: "User created successfully",
       user: {
-        id: newUser._id,
-        email: newUser.email,
+        id: user._id,
+        email: user.email,
       },
     });
   }
---
+++ backend/src/services/regsiterService.ts
@@ -0,0 +1,48 @@
+import expressAsyncHandler from "express-async-handler";
+import { INTERNAL_SERVER_ERROR, NOT_FOUND } from "../consts/http.codes";
+import HTTP_Error from "../utils/httpError";
+import Admin from "../model/users/admin.model";
+import Merchant from "../model/users/merchant.model";
+import Customer from "../model/users/customer.model";
+
+const modelNames = {
+  admin: "Admin",
+  customer: "Customer",
+  merchant: "Merchant",
+};
+
+export const registerService = async (Model: any, registerBody: any) => {
+  const { firstName, lastName, email } = registerBody;
+  console.log(registerBody);
+
+  const existingUser = await Model.findOne({ email });
+  if (existingUser) {
+    throw new HTTP_Error("User already exists", INTERNAL_SERVER_ERROR);
+  }
+
+  const generatedPassword = "123";
+  const newUser = { firstName, lastName, email, password: generatedPassword };
+  let user;
+
+  if (Model.modelName === modelNames.admin) {
+    user = await Admin.create({
+      ...newUser,
+    });
+  }
+  if (Model.modelName === modelNames.merchant) {
+    user = await Merchant.create({
+      ...newUser,
+    });
+  }
+  if (Model.modelName === modelNames.customer) {
+    user = await Customer.create({
+      ...newUser,
+    });
+  }
+
+  if (!user) {
+    throw new HTTP_Error("Failed to create user", INTERNAL_SERVER_ERROR);
+  }
+
+  return user;
+};
